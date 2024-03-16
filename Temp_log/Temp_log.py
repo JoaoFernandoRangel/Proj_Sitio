393e8fe3d6e868f21e0d7fa3186c3efcd0afe53f
João Fernando Rangel <122740076+JoaoFernandoRangel@users.noreply.github.com>
2024-03-16 16:29:26 -0300

    Diretórios
    Ajuste de configuração de diretórios

--- Temp_log/Temp_log.py
+++ Temp_log/Temp_log.py
@@ -1,15 +1,21 @@
-import os
+import sys
 import paho.mqtt.client as paho
-import time
 from datetime import datetime
 
-global mensagem
-diretorio = "C:\\Users\\João Fernando Rangel\\Desktop\\Proj_Sitio\\Temp_log\\"
+print(sys.executable)
+pc = "jf1"  #altere caso troque de computador
+
+if (pc == "jf"):
+    diretorio = "C:\\Users\\João Fernando Rangel\\Desktop\\Proj_Sitio\\Temp_log\\"
+else:
+    diretorio = "C:\\Users\\Beatriz\\Documents\\GitHub\\Proj_Sitio\\Temp_log\\"
+
 data_hora_atual = datetime.now()
 formato_data_hora = "%Y-%m-%d_%H-%M-%S"
 timestamp = data_hora_atual.strftime(formato_data_hora)
 nome_arquivo = diretorio + timestamp + "__ Dados de temperatura.txt"
-arquivo = open(nome_arquivo, "w+")
+arquivo = open(nome_arquivo, "w")
+arquivo.write("Teste\n")
 #arquivo.close()
 
 def on_connect(client, userdata, flags, rc, properties=None):
@@ -27,7 +33,6 @@ def on_message(client, userdata, msg):
     # Process the received message here instead of just printing
     print("Received message:", mensagem)
     listaa = mensagem.split("-")
-    #print(listaa)
     arquivo.write(listaa[3] + space + listaa[2] + space+ listaa[4] + space + listaa[5] + "ºC"+ "\n")
     #arquivo.close()
 
